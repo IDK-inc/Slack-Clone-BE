ce143f9f6a9309b2f0ddffa49a8e77740aca479b
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-10-20 06:53:06 -0500

    starting to work on some of the tables

---
+++ db/migrations/20191020064735_Users_Table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('Users_Table', (Table) => {
+      Table.increments();
+
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('Users_Table');
+};
